a0f99f404cd5bd5b263bff386a2f9c647e9b2fea
shotarich <shotarich@163.com>
2019-03-24 20:33:17 +0800

    modify common.js

--- CommonJS/index.js
+++ CommonJS/index.js
@@ -7,26 +7,28 @@ function include(path) {
     path = path.endsWith('.js') ? path : path + '.js'; // path = path.slice(-3) === '.js' ? path : path + '.js';
     const content = fs.readFileSync(path, 'utf8');
 
+    let module = {
+        exports: {}
+    };
+
     let resFn = new Function('exports', 'module', 'require', '__dirname', '__filename',
-        content + '\n return module.exports'
+        content
     );
     /* 
         最终resFn这个函数是这样
         function resFn(exports, module, include, __dirname, __filename) {
+            // 下面的内容是require进来的content
             function add(a, b) {
                 return a + b;
             }
             module.exports = add;
-
-            return module.exports;
         }
     */
 
-    let module = {
-        exports: {}
-    }
 
-    return resFn(module.exports, module, include, __dirname, __filename);
+    resFn(module.exports, module, include, __dirname, __filename);
+
+    return module.exports;
 }
 
 const add = include('./mod');
